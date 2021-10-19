e8f7dd44adcea8ca58086a509788be9bb3fa654e
Roland-Shum <rolandshum.shc@gmail.com>
2021-10-19 19:49:05 +0300

    Edited contents

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export const Index = ({ posts }: IndexProps): JSX.Element => {
         href="https://github.com/rolandshc/personalBlogNextjs"
         className="inline-block px-7 py-3 rounded-md text-white dark:text-white bg-blue-600 hover:bg-blue-700 hover:text-white dark:hover:text-white"
       >
-        Get the source code! Test
+        Get the source code
       </a>
 
       {posts.map((post) => (
