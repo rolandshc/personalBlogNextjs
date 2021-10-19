b7b83836d6ef426d02f3a23d2dd937241bf0ed95
Roland-Shum <rolandshum.shc@gmail.com>
2021-10-19 18:44:39 +0300

    first commit

---
+++ pages/about.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+export const About = (): JSX.Element => {
+  return (
+    <Layout
+      customMeta={{
+        title: 'About - Roland Shum',
+      }}
+    >
+      <h1>About me</h1>
+      <p>Based in Tallinn, Estonia. From Hong Kong originally.</p>
+      <p>Active in IT field - system design / XR / Medical Software</p>
+      <p>A beliver. A front liner. Tanky and try-hard.
+        Yet it is short, focus on what I could do and get in touch with me to know more.
+</p>
+
+   
+    </Layout>
+  );
+};
+
+export default About;
